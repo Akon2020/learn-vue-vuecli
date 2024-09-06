0747a8324e6e21c16e1ccffecf9ecdda2f03a2c9
Akonkwa Ushindi Isaac <akonkwaushindi@gmail.com>
2024-09-06 07:18:42 +0200

    learn onClick event

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <script>
 export default {
-  data(){
-    return{
-      name: "Akonkwa Ushindi",
+  data() {
+    return {
+      name: 'Akonkwa Ushindi',
       status: 'active',
       tasks: ['task one', 'task two', 'task three'],
-      link: "https://google.com",
-    };
+      link: 'https://google.com'
+    }
   },
-};
+  methods: {
+    toogleStatus() {
+      if (this.status === 'active') {
+        this.status = 'pending  '
+      } else if (this.status === 'pending') {
+        this.status = 'inactive'
+      } else {
+        this.status = 'active'
+      }
+    }
+  }
+}
 </script>
 
 <template>
@@ -18,7 +29,8 @@ export default {
   <p v-else>User inactive</p>
   <h3>tasks:</h3>
   <ul>
-    <li v-for="task, i in tasks" :key="task">{{i+1}}. {{ task }}</li>
+    <li v-for="(task, i) in tasks" :key="task">{{ i + 1 }}. {{ task }}</li>
   </ul>
   <a :href="link">Click here for Google</a>
+  <button @click="toogleStatus">Click here for change status</button>
 </template>
