5043beb058bd474bdbe9bfb88b79c430b5b60e11
Akonkwa Ushindi Isaac <akonkwaushindi@gmail.com>
2024-09-06 13:26:28 +0200

    learn form submission

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ const name = ref('Akonkwa Ushindi')
 const status = ref('active')
 const tasks = ref(['Task one', 'Task two', 'Task three'])
 const link = ref('https://google.com')
+const newTask = ref('')
 const toogleStatus = () => {
   if (status.value === 'active') {
     status.value = 'pending  '
@@ -14,6 +15,12 @@ const toogleStatus = () => {
     status.value = 'active'
   }
 }
+const addTask = () => {
+  if (newTask.value.trim() != '') {
+    tasks.value.push(newTask)
+    newTask.value = ''
+  }
+}
 </script>
 
 <template>
@@ -21,6 +28,11 @@ const toogleStatus = () => {
   <p v-if="status === 'active'">User active</p>
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User inactive</p>
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask" />
+    <button type="submit">Submit</button>
+  </form>
   <h3>tasks:</h3>
   <ul>
     <li v-for="(task, i) in tasks" :key="task">{{ i + 1 }}. {{ task }}</li>
